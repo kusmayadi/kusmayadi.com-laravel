e942fe994db6e7403fced96f5f7c3cc06aaa7ee3
Kusmayadi <kusmayadi@gmail.com>
2021-09-28 20:23:10 +0700

    Add work experience page

---
+++ resources/views/page/work-experience.blade.php
@@ -0,0 +1,128 @@
+@extends('layout', ['image' => $unsplashImage])
+
+@section('content')
+    <h1 class="text-5xl text-yellow-600 filter drop-shadow-md mt-8">Work Experience</h1>
+
+    <div class="mt-10">
+        <div class="mb-4">
+            <h1 class="text-2xl">Senior Developer</h1>
+            <h2 class="text-lg">VE Capital Asia</h2>
+            <h3><i class="fas fa-globe text-gray-400"></i> <a href="https://vecapital.asia/">vecapital.asia</a></h3>
+            <div>Jul 2020 – Present</div>
+            <div>Remote - Singapore</div>
+        </div>
+
+        <div class="mb-4">
+            <h1 class="text-2xl">CTO</h1>
+            <h2 class="text-lg">PT Cerita Teknologi Indonesia</h2>
+            <h3><i class="fas fa-globe text-gray-400"></i> <a href="https://restock.id">restock.id</a></h3>
+            <div>Sep 2019 – Feb 2020</div>
+            <div>Jakarta, Indonesia</div>
+        </div>
+
+        <div class="mb-4">
+            <h1 class="text-2xl">CTO</h1>
+            <h2 class="text-lg">PT. Brodo Ganesha Indonesia</h2>
+            <h3><i class="fas fa-globe text-gray-400"></i> <a href="https://bro.do">bro.do</a></h3>
+            <div>Dec 2016 – Aug 2019</div>
+            <div>Jakarta, Indonesia</div>
+        </div>
+
+        <div class="mb-4">
+            <h1 class="text-2xl">Lead Developer</h1>
+            <h2 class="text-lg">PT Bursa Interaktif Gemilang</h2>
+            <h3><i class="fas fa-globe text-gray-400"></i> <a href="https://brambang.com">brambang.com</a></h3>
+            <div>Oct 2016 – Nov 2016</div>
+            <div>Jakarta, Indonesia</div>
+        </div>
+
+        <div class="mb-4">
+            <h1 class="text-2xl">Lead Developer</h1>
+            <h2 class="text-lg">PT Pikmiap Ekspres Global</h2>
+            <h3><i class="fas fa-globe text-gray-400"></i> <a href="https://pikmiap.com">pikmiap.com</a></h3>
+            <div>Apr 2016 – Sep 2016</div>
+            <div>Jakarta, Indonesia</div>
+        </div>
+
+        <div class="mb-4">
+            <h1 class="text-2xl">CTO</h1>
+            <h2 class="text-lg">PT. Laju Video Media</h2>
+            <h3><i class="fas fa-globe text-gray-400"></i> <a href="https://lv8.tv">lv8.tv</a></h3>
+            <div>Nov 2013 – Apr 2016</div>
+            <div>Jakarta, Indonesia</div>
+        </div>
+
+        <div class="mb-4">
+            <h1 class="text-2xl">Manager - Web Developer</h1>
+            <h2 class="text-lg">PT. Kreatif Media Karya</h2>
+            <h3><i class="fas fa-globe text-gray-400"></i> <a href="https://www.kmkonline.co.id">kmkonline.co.id</a> / <a href="https://liputan6.com">liputan6.com</a></h3>
+            <div>Jul 2013 – Oct 2013</div>
+            <div>Jakarta, Indonesia</div>
+        </div>
+
+        <div class="mb-4">
+            <h1 class="text-2xl">Supervisor, Mid Developer</h1>
+            <h2 class="text-lg">Multiply</h2>
+            <h3><i class="fas fa-globe text-gray-400"></i> <a href="https://multiply.com">multiply.com</a></h3>
+            <div>Dates EmployedSep 2012 – May 2013</div>
+            <div>Jakarta, Indonesia</div>
+        </div>
+
+        <div class="mb-4">
+            <h1 class="text-2xl">Developer (Part Time)</h1>
+            <h2 class="text-lg">PT Trijaya Dewata</h2>
+            <h3><i class="fas fa-globe text-gray-400"></i> <a href="http://trijayadewata.com/">trijayadewata.com</a> / <a href="https://www.baliplus.com/">baliplus.com</a></h3>
+            <div>Aug 2011 – Aug 2012</div>
+            <div>Denpasar, Bali, Indonesia</div>
+        </div>
+
+        <div class="mb-4">
+            <h1 class="text-2xl">IT Developer</h1>
+            <h2 class="text-lg">Kemana.com</h2>
+            <h3><i class="fas fa-globe text-gray-400"></i> <a href="https://kemana.com">kemana.com</a></h3>
+            <div>Nov 2009 – Jun 2010</div>
+            <div>Denpasar, Bali, Indonesia</div>
+        </div>
+
+        <div class="mb-4">
+            <h1 class="text-2xl">Web Programmer (Part Time)</h1>
+            <h2 class="text-lg">PT Baru di Bali</h2>
+            <h3><i class="fas fa-globe text-gray-400"></i> <a href="http://barudibali.com">barudibali.com</a></h3>
+            <div>Jul 2009 – Oct 2009</div>
+            <div>Denpasar, Bali, Indonesia</div>
+        </div>
+
+        <div class="mb-4">
+            <h1 class="text-2xl">Web Developer (Contract)</h1>
+            <h2 class="text-lg">Harver Group, LLC</h2>
+            <h3><i class="fas fa-globe text-gray-400"></i> <a href="https://saywire.com">saywire.com</a></h3>
+            <div>Aug 2006 – May 2009</div>
+            <div>Remote (with US based company)</div>
+        </div>
+
+        <div class="mb-4">
+            <h1 class="text-2xl">Web Programmer (Part Time)</h1>
+            <h2 class="text-lg">ABL Tours and Travel</h2>
+            <h3><i class="fas fa-globe text-gray-400"></i> <a href="https://baliwww.com/">baliwww.com</a></h3>
+            <div>Jun 2008 – Aug 2008</div>
+            <div>Denpasar, Bali, Indonesia</div>
+        </div>
+
+        <div class="mb-4">
+            <h1 class="text-2xl">Various Positions</h1>
+            <h2 class="text-lg">PT Agranet Multicitra Siberkom</h2>
+            <h3><i class="fas fa-globe text-gray-400"></i> <a href="https://detik.com">detik.com</a></h3>
+            <div>Jakarta, Indonesia</div>
+            <h4>Research & Development Supervisor</h4>
+            <div>Jun 2005 – Aug 2006</div>
+            <h4>Mobile Platform Development Supervisor</h4>
+            <div>Jan 2003 - Jun 2005</div>
+            <h4>Online Platform Development Supervisor</h4>
+            <div>Feb 2001 - Dec 2002</div>
+            <h4>Senior Programmer</h4>
+            <div>Jan 2000 – Jan 2001</div>
+            <h4>Programmer</h4>
+            <div>Nov 1995 – Dec 1999</div>
+        </div>
+    </div>
+@endsection

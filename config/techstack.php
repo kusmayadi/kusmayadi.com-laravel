0b6ff3746e8b097b794870f32062bbf8738e217a
Kusmayadi <kusmayadi@gmail.com>
2021-09-29 19:53:38 +0700

    Add tech stack page

---
+++ config/techstack.php
@@ -0,0 +1,195 @@
+<?php
+
+return [
+    [
+        'name' => 'PHP',
+        'image' => 'php.svg',
+        'url' => 'https://www.php.net/'
+    ],
+
+    [
+        'name' => 'Laravel',
+        'image' => 'laravel.svg',
+        'url' => 'https://laravel.com/'
+    ],
+
+    [
+        'name' => 'Composer',
+        'image' => 'composer.svg',
+        'url' => 'https://getcomposer.org/'
+    ],
+
+    [
+        'name' => 'Node.js',
+        'image' => 'nodejs.svg',
+        'url' => 'https://nodejs.org/'
+    ],
+
+    [
+        'name' => 'npm',
+        'image' => 'npm.svg',
+        'url' => 'https://www.npmjs.com/'
+    ],
+
+    [
+        'name' => 'Sails',
+        'image' => 'sails.svg',
+        'url' => 'https://sailsjs.com/'
+    ],
+
+    [
+        'name' => 'Vue.js',
+        'image' => 'vue.svg',
+        'url' => 'https://vuejs.org/'
+    ],
+
+    [
+        'name' => 'Nuxt',
+        'image' => 'nuxt.svg',
+        'url' => 'https://nuxtjs.org/'
+    ],
+
+    [
+        'name' => 'jQuery',
+        'image' => 'jquery.svg',
+        'url' => 'https://jquery.com/'
+    ],
+
+    [
+        'name' => 'tailwindcss',
+        'image' => 'tailwindcss-icon.svg',
+        'url' => 'https://tailwindcss.com/'
+    ],
+
+    [
+        'name' => 'Bootstrap',
+        'image' => 'bootstrap.svg',
+        'url' => 'https://getbootstrap.com/'
+    ],
+
+    [
+        'name' => 'MySQL',
+        'image' => 'mysql.svg',
+        'url' => 'https://www.mysql.com/'
+    ],
+
+    [
+        'name' => 'redis',
+        'image' => 'redis.svg',
+        'url' => 'https://redis.io/'
+    ],
+
+    [
+        'name' => 'AWS',
+        'image' => 'aws.svg',
+        'url' => 'https://aws.amazon.com/'
+    ],
+
+    [
+        'name' => 'DigitalOcean',
+        'image' => 'digital-ocean.svg',
+        'url' => 'https://www.digitalocean.com/'
+    ],
+
+    [
+        'name' => 'NGINX',
+        'image' => 'nginx.svg',
+        'url' => 'https://www.nginx.com/'
+    ],
+
+    [
+        'name' => 'Apache HTTP Server',
+        'image' => 'apache.svg',
+        'url' => 'https://httpd.apache.org/'
+    ],
+
+    [
+        'name' => 'Ansible',
+        'image' => 'ansible.svg',
+        'url' => 'https://www.ansible.com/'
+    ],
+
+    [
+        'name' => 'Vagrant',
+        'image' => 'vagrant.svg',
+        'url' => 'https://www.vagrantup.com/'
+    ],
+
+    [
+        'name' => 'Visual Studio Code',
+        'image' => 'visual-studio-code.svg',
+        'url' => 'https://code.visualstudio.com/'
+    ],
+
+    [
+        'name' => 'Vim',
+        'image' => 'vim.svg',
+        'url' => 'https://www.vim.org/'
+    ],
+
+    [
+        'name' => 'Atom',
+        'image' => 'atom.svg',
+        'url' => 'https://atom.io/'
+    ],
+
+    [
+        'name' => 'git',
+        'image' => 'git-icon.svg',
+        'url' => 'https://git-scm.com/'
+    ],
+
+    [
+        'name' => 'GitHub',
+        'image' => 'github-icon.svg',
+        'url' => 'https://github.com/'
+    ],
+
+    [
+        'name' => 'GitLab',
+        'image' => 'gitlab.svg',
+        'url' => 'https://about.gitlab.com/'
+    ],
+
+    [
+        'name' => 'Bitbucket',
+        'image' => 'bitbucket.svg',
+        'url' => 'https://bitbucket.org/'
+    ],
+
+    [
+        'name' => 'Pivotal Tracker',
+        'image' => 'pivotal_tracker.svg',
+        'url' => 'https://www.pivotaltracker.com/'
+    ],
+
+    [
+        'name' => 'Trello',
+        'image' => 'trello.svg',
+        'url' => 'https://trello.com/'
+    ],
+
+    [
+        'name' => 'Slack',
+        'image' => 'slack-icon.svg',
+        'url' => 'https://slack.com/'
+    ],
+
+    [
+        'name' => 'macOS',
+        'image' => 'macOS.svg',
+        'url' => 'https://www.apple.com/id/macos'
+    ],
+
+    [
+        'name' => 'Fedora',
+        'image' => 'fedora.svg',
+        'url' => 'https://getfedora.org/'
+    ],
+
+    [
+        'name' => 'Ubuntu',
+        'image' => 'ubuntu.svg',
+        'url' => 'https://ubuntu.com/'
+    ],
+];
---
+++ resources/views/page/tech-stack.blade.php
@@ -0,0 +1,15 @@
+@extends('layout', ['image' => $unsplashImage, 'title' => 'Tech Stack'])
+
+@section('content')
+    <h1 class="text-5xl text-yellow-600 filter drop-shadow-md mt-8">Tech Stack</h1>
+
+    <div class="mt-10 grid grid-cols-4 gap-20">
+        @foreach (config('techstack') as $stack)
+            <div>
+                <a href="{{ $stack['url'] }}" target="techstack">
+                    <img src="{{ asset('images/' . $stack['image']) }}" alt="{{ $stack['name'] }}" class="object-center h-20 w-full">
+                </a>
+            </div>
+        @endforeach
+    </div>
+@endsection

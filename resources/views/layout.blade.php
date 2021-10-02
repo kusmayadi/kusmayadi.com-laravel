7e2b28941fcfe3703cb3d6a07a64028aae5ebf24
Kusmayadi <kusmayadi@gmail.com>
2021-10-02 11:25:31 +0700

    Update layout, change rounded

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -24,11 +24,11 @@
         <div x-data="{ open: false }" class="h-screen w-screen bg-cover bg-no-repeat bg-center" style="background-image: url({{ asset('storage/' . $image->name) }})">
 
             <div class="py-2 px-4 flex justify-between items-center">
-                <div @click.away="open = false" class="flex items-center bg-opacity-70 text-yellow-600 bg-black px-3 lg:px-6 py-2 rounded lg:rounded-full font-bold text-xs lg:text-xl">
+                <div @click.away="open = false" class="flex items-center bg-opacity-70 text-yellow-600 bg-black px-3 lg:px-6 py-2 rounded-md font-bold text-xs lg:text-xl">
                     <div class="lg:hidden mr-2">
                         <i @click="open = !open" class="fas fa-bars"></i>
 
-                        <ul x-show="open" class="fixed left-4 mt-3 bg-black bg-opacity-90 z-50 text-white rounded px-4 py-3 text-xs font-medium leading-6">
+                        <ul x-show="open" class="fixed left-4 mt-3 bg-black bg-opacity-90 z-50 text-white rounded-md px-4 py-3 text-xs font-medium leading-6">
                             <li>
                                 <a href="{{ route('page', ['page' => 'work-experience']) }}" class="{{ (\Arr::exists(\Route::current()->parameters, 'page') AND \Route::current()->parameters['page'] === 'work-experience') ? 'active' : '' }}">
                                     <i class="fas fa-briefcase mr-1"></i>Work Experience
@@ -65,7 +65,7 @@
                 </div>
 
                 <div>
-                    <ul class="hidden lg:flex bg-black bg-opacity-70 text-white rounded-full px-4 py-2 text-sm font-medium">
+                    <ul class="hidden lg:flex bg-black bg-opacity-70 text-white rounded-md px-4 py-2 text-sm font-medium">
                         <li>
                             <a href="{{ route('page', ['page' => 'work-experience']) }}" class="{{ (\Arr::exists(\Route::current()->parameters, 'page') AND \Route::current()->parameters['page'] === 'work-experience') ? 'active' : '' }}">
                                 <i class="fas fa-briefcase mr-1"></i>Work Experience
@@ -101,7 +101,7 @@
             </div>
 
             @if (\Arr::exists(\Route::current()->parameters, 'page'))
-            <div class="bg-white mx-auto w-3/4 lg:w-1/2 h-3/4 px-10 py-8 bg-opacity-90 mt-4 lg:mt-16 rounded-lg scrollbar-thin scrollbar-thumb-yellow-600 scrollbar-track-gray-200 scrollbar-track-rounded-lg scrollbar-thumb-rounded-lg overflow-y-scroll shadow-xl">
+            <div class="bg-white mx-auto w-3/4 lg:w-1/2 h-3/4 px-10 py-8 bg-opacity-90 mt-4 lg:mt-16 rounded-lg scrollbar-thin scrollbar-thumb-yellow-600 scrollbar-track-gray-200 scrollbar-track-rounded-lg scrollbar-thumb-rounded-lg overflow-y-scroll shadow-2xl">
                 @yield('content')
             </div>
             @endif
